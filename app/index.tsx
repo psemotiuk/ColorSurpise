14633533a1ff6d5c6ffd209f7321a786cff59d6e
Pavlo Semotiuk <pavlo.semotiuk@sapient.pro>
2025-01-26 23:10:07 +0100

    implemented current color changing

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Pressable, StyleSheet, Text } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
-export default function Index() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={styles.text}>Hello there</Text>
-    </View>
+const RGB_MAX_VALUE = 256;
+const ANIMATION_DURATION = 500;
+
+const initialBgColorArray = [RGB_MAX_VALUE, RGB_MAX_VALUE, RGB_MAX_VALUE];
+
+const toRgbString = (arrayOfColors: number[]): string => {
+  return `rgb(${arrayOfColors.join(",")})`;
+};
+
+const Index = () => {
+  const currentColorAnimated = useSharedValue<string>(
+    toRgbString(initialBgColorArray)
   );
-}
+
+  const [currentColor, setCurrentColor] = useState<string>(
+    toRgbString(initialBgColorArray)
+  );
+
+  const handleChangeBg = () => {
+    const rgbColorArray = Array.from({ length: 3 }, () =>
+      Math.floor(RGB_MAX_VALUE * Math.random())
+    );
+    const nextRandomColor = toRgbString(rgbColorArray);
+    setCurrentColor(nextRandomColor);
+    currentColorAnimated.value = withTiming(nextRandomColor, {
+      duration: ANIMATION_DURATION,
+    });
+  };
+
+  const bgColorChangeAnimated = useAnimatedStyle(() => {
+    return {
+      backgroundColor: currentColorAnimated.value,
+    };
+  }, []);
+
+  return (
+    <Pressable style={{ flex: 1 }} onPress={handleChangeBg}>
+      <Animated.View
+        style={[
+          styles.container,
+          { backgroundColor: currentColor },
+          bgColorChangeAnimated,
+        ]}
+      >
+        <Text style={styles.text}>Hello there</Text>
+      </Animated.View>
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 32
-    }
-})
+  text: {
+    fontSize: 32,
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default Index;
